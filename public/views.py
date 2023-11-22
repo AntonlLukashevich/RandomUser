ae26218cf36f4c9aa88e758436a2951c8712bed5
AntonlLukashevich <bender1850@gmail.com>
2023-11-22 19:36:08 +0300

    last commit

--- public/views.py
+++ public/views.py
@@ -3,38 +3,16 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 import requests
 
-from .models import RandomUser
+from .serializers import RandomUserSerializer
 
 
 class RandomUserViewSet(APIView):
     api_url = "https://randomuser.me/api/"
     permission_classes = [AllowAny]
+    serializer_class = RandomUserSerializer
 
     def post(self, request):
-        response = requests.get(self.api_url).json()
+        response = requests.get(self.api_url)
+        self.serializer_class.save_response(response.json(), response_data=response.json())
 
-        result = response["results"][0]
-        name = result['name']
-        location = result['location']
-        login = result['login']
-        dob = result['dob']
-
-        data = {
-            "gender": result.get("gender"),
-            "first_name": name.get("first"),
-            "last_name": name.get("last"),
-            "street_number": location['street'].get("number"),
-            "street_name": location['street'].get("name"),
-            "city": location.get("city"),
-            "country": location.get("country"),
-            "postcode": str(location.get("postcode")),
-            "login": login.get("username"),
-            "password": login.get("password"),
-            "email": result.get("email"),
-            "born_date": dob.get("date"),
-            "age": dob.get("age"),
-        }
-
-        RandomUser.objects.create(**data)
-
-        return Response(data)
+        return Response({"status": "success"})
